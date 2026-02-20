c27d1058d6adb7f947cad521061dc2975a4b773b
Ced <cedricvincenth@yahoo.com>
2026-02-20 22:21:21 +0800

    added state

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,26 +1,40 @@
+import { useState, type SubmitEvent } from "react"
 import MovieCard from "../components/MovieCard"
 import type { Movie } from "../models/Movie"
 
 function Home() {
+    const [searchQuery, setSearchQuery] = useState("")
+
     const movies : Movie[] = [
         {id: 1, title: "John Wick", release_date: "2020"},
         {id: 2, title: "Terminator", release_date: "1998"},
         {id: 3, title: "The Matrix", release_date: "1999"}
     ]
 
-    const handleSearch = () => {
-
+    const handleSearch = (e : SubmitEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
     }
 
     return (
         <div className="home">
             <form onSubmit={handleSearch} className="search-form">
-                <input type="text" placeholder="Search for movies..." className="search-input" />
+                <input 
+                    type="text" 
+                    placeholder="Search for movies..." 
+                    className="search-input"
+                    value={searchQuery}    
+                    onChange={e => setSearchQuery(e.target.value)}
+                />
                 <button type="submit" className="search-btn">Search</button>
             </form>
 
             <div className="movies-grid">
-                {movies.map(movie => <MovieCard movie={movie} key={movie.id}/>)}
+                {
+                    movies.map(movie => 
+                        movie.title.toLowerCase().includes(searchQuery) && <MovieCard movie={movie} key={movie.id}/>)
+                }
             </div>
         </div>
     )
