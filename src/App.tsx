ed551a0f34289c18aa50db49010f8b63f62b2ca3
Ced <cedricvincenth@yahoo.com>
2026-02-22 13:16:30 +0800

    page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
+import Favorites from './pages/Favorites'
+import NavBar from './components/NavBar'
 
 function App() {
 
   return (
-    <>
-      <Home></Home>
-    </>
+    <div>
+      <NavBar />
+      <main className='main-content'>
+        <Routes>
+          <Route path='/' element={ <Home/> } />
+          <Route path='/favorites' element={ <Favorites/> } />
+        </Routes>
+      </main>
+    </div>
   )
 }
 
